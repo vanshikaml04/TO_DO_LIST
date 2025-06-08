7c986543c352c64baab9f4194bdb99450984ba4e
vanshikaml04 <ranivanshika849@gmail.com>
2025-06-08 07:54:59 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask(){
+    if(inputBox.value===''){
+        alert('kuch te duss 🤌');
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "🗑️";
+        span.classList.add("delete-btn");
+        li.appendChild(span);
+        
+    }
+    inputBox.value = "";
+    savedata();
+}
+
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("checked");
+        savedata();
+    }
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+        savedata();
+    }
+},false);
+
+function savedata(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
